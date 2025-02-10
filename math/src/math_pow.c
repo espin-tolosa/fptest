5da59e13a00dae0046536f9dbb9711ed7a4aa9a2
espin-tolosa <samuel.espin-tolosa+github@gmail.com>
2025-02-10 19:20:46 +0100

    Dev: simplify math_pow

--- math/src/math_pow.c
+++ math/src/math_pow.c
@@ -22,93 +22,32 @@ extern f64_t math_pow(f64_t x, f64_t y)
         {
             if( x > 0.0 )
             {
-                if( x == 1.0 )
+                static const f128_t rthalf   = 0.70710678118654752440084436210485L;
+                dnorm_t xn = math_cwnormalize( x, result.type );
+
+                if ( xn.f.d < rthalf )
                 {
-                    result.f.d = 1.0;
+                    xn.f.d  = 2.L * xn.f.d;
+                    xn.e    = xn.e - 1;
                 }
 
-                else if( math_intrnd( y ) == y )
-                {
-                    result.f.d = math_pow_i16( x, y );
-                }
+                const f128_t z = y * xn.e;
 
-                else
-                {
-                    const dnorm_t xn = math_cwnormalize( x, result.type );
-                    const f128_t z = y * xn.e;
-
-                    if( z < -1074.L )
-                    {
-                        result.f.w[ W0 ] = ZERO;
-                    }
-
-                    else if( z > 1025.5L )
-                    {
-                        result.f.w[ W0 ] = INF;
-                    }
-                    else
-                    {
-                        result.f.d = math_pow_impl( xn, y );
-                    }
-                }
+                const i16_t zl  = math_to_integer( y * ( f128_t ) xn.e );
+                const f64_t n   = math_to_integer( y );
+                const f128_t yx = math_pow_ydexp( zl, y, ( f128_t ) xn.e ) + ( ( y - ( f128_t ) n ) * math_log( xn.f.d ) );
+                result.f.d      = math_pow_i16( xn.f.d, (i16_t) n ) * math_exp( yx ) * math_cwsetexp( 0.5, zl );
             }
 
             else
             {
-                if( math_intrnd( y ) == y )
-                {
-                    if( (i128_t)y%2==0 )
-                    {
-                        result.f.d = math_pow_i16( -x, y );
-                    }
-
-                    else
-                    {
-                        result.f.d = -math_pow_i16( -x, y );
-                    }
-                }
-
-                else
-                {
-                    result.f.w[ W0 ] = NIL;
-                }
+                result.f.w[ W0 ] = NIL;
             }
 
             break;
         }
 
         case (INF):
-        {
-            const f64_t iy = math_intrnd(y);
-
-            if( y == 0.0 )
-            {
-                result.f.d = 1.0;
-            }
-
-            else if( (x > 0.0) && (y < 0.0) )
-            {
-                result.f.d = +0.0;
-            }
-
-            else if( (x < 0.0) && (y < 0.0) && (iy == y) && ( (i128_t) iy % 2 != 0 ) )
-            {
-                result.f.w[ W0 ] = SMASK;
-            }
-
-            else if( (x < 0.0) && (y > 0.0) && (iy == y) && ( (i128_t) iy % 2 != 0 ) )
-            {
-                result.f.w[ W0 ] = SMASK | result.f.w[ W0 ];
-            }
-
-            else
-            {
-                /* NOP: already +inf */
-            }
-
-            break;
-        }
-
         case (NIL):
         {
             result.f.w[ W0 ] = NIL;
@@ -117,66 +56,14 @@ extern f64_t math_pow(f64_t x, f64_t y)
 
         case (ZERO): /* x^0 = 1, for all x >= 0, even 0^0 */
         {
-            const f64_t iy = math_intrnd(y);
-
-            if( y == 0.0 )
+            if( y > 0.0 )
             {
-                result.f.d = +1.0;
+                result.f.w[ W0 ] = ZERO;
             }
 
-            else if( (y > 0.0) && (iy == y) && ( (i128_t) iy % 2 != 0 ) )
+            else
             {
-                if( y == 1.0 )
-                {
-                    result.f.d = x;
-
-                    if( result.f.w[ W0 ] & SMASK )
-                    {
-                        result.f.w[ W0 ] = SMASK;
-                    }
-
-                    else
-                    {
-                        result.f.w[ W0 ] = ZERO;
-                    }
-
-                }
-                else
-                {
-                    result.f.d = -0.0;
-                }
-            }
-
-            else if( y > 0.0 )
-            {
-                result.f.d = +0.0;
-            }
-
-            else if( (y < 0.0) && (iy == y) && ( (i128_t) iy % 2 != 0 ) )
-            {
-                if( y == -1.0 )
-                {
-                    result.f.d = x;
-
-                    if( result.f.w[ W0 ] & SMASK )
-                    {
-                        result.f.w[ W0 ] = INF | SMASK;
-                    }
-
-                    else
-                    {
-                        result.f.w[ W0 ] = INF;
-                    }
-                }
-                else
-                {
-                    result.f.w[ W0 ] = SMASK | INF;
-                }
-            }
-
-            else if( y < 0.0 )
-            {
-                result.f.w[ W0 ] = INF;
+                result.f.w[ W0 ] = NIL;
             }
 
             break;
@@ -213,7 +100,7 @@ static i16_t math_pow_safe_exp( f128_t y )
 
 static f128_t math_pow_ydexp( i64_t zl, f128_t y, f128_t dexp )
 {
-    static const f128_t ln2 = 0.69314718055994530942L;
+    static const f128_t ln2 = 0.69314718055994530941723212145818L;
 
     f128_t result;
 
@@ -234,51 +121,13 @@ static f128_t math_pow_ydexp( i64_t zl, f128_t y, f128_t dexp )
 
 static f128_t math_pow_impl( dnorm_t xn, f128_t y )
 {
-    static const f128_t rthalf   = 0.70710678118654752440L;
+    const i16_t zl = math_to_integer( y * ( f128_t ) xn.e );
 
-    f128_t yi   = math_to_integer( y );
+    const f64_t n = math_to_integer( y );
 
-    if ( xn.f.d < rthalf )
-    {
-        xn.f.d  = 2.L * xn.f.d;
-        xn.e    = xn.e - 1;
-    }
+    const f128_t yx = math_pow_ydexp( zl, y, ( f128_t ) xn.e ) + ( ( y - ( f128_t ) n ) * math_log( xn.f.d ) );
 
-    const f128_t dexp = ( f128_t ) xn.e;
-
-    const i64_t zl = ( i64_t ) ( y * dexp );
-
-    f128_t yx = math_pow_ydexp( zl, y, dexp );
-
-    i16_t zexp = ( i16_t ) math_pow_clamp( zl, -1074, +1024 );
-
-    const i16_t n = math_pow_safe_exp( y );
-
-    yi = y - ( f128_t ) n;
-
-    if ( yi != 0.0L )
-    {
-        yx = yx + ( yi * math_log( xn.f.d ) );
-    }
-
-    else
-    {
-        /* NOP: not need to compute math_log( xn ) when yi = 0 */
-    }
-
-    f128_t z = math_pow_i16( xn.f.d, n );
-
-    if ( yx != 0.0L )
-    {
-        z = z * math_exp( yx );
-    }
-
-    else
-    {
-        z = 1.0L;
-    }
-
-    z = z * (f128_t) math_cwsetexp( 0.5, zexp );
+    const f128_t z = math_pow_i16( xn.f.d, (i16_t) n ) * math_exp( yx ) * math_cwsetexp( 0.5, zl );
 
     return ( z );
 }
@@ -326,28 +175,6 @@ static f128_t math_pow_i16( f128_t x, i16_t n )
     if ( n < 0 )
     {
         result = 1.0 / result;
-    }
-
-    return ( result );
-}
-
-static i64_t math_pow_clamp( i64_t x, i64_t xmin, i64_t xmax )
-{
-    i64_t result;
-
-    if( x <= xmin )
-    {
-        result = xmin;
-    }
-
-    else if ( x < xmax )
-    {
-        result = x;
-    }
-
-    else
-    {
-        result = xmax;
     }
 
     return ( result );
